86d2a14cb21fe7677f8b666b8cce1644cb760b6b
Leandro Toledo <leandrotoledo@Leandros-MacBook-Pro.local>
2023-11-06 17:30:33 +0000

    Adicionando navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from 'next'
+import Navbar from './components/Navbar';
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Next E-commerce 13',
   description: 'Next E-commerce utilizando a versao 13',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main className='bg-slate-700 h-screen p-16'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
